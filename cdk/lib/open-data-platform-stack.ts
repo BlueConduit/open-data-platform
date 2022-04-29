f8d519280a1e01d57ddcb5158e37344ae96289de
Justine Breuch <breuch@google.com>
2022-04-29 13:31:58 -0400

    Add hello world Vue app and cdk initialized skeleton

---
+++ cdk/lib/open-data-platform-stack.ts
@@ -0,0 +1,10 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class OpenDataPlatformStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // TODO(fellows): The code that defines your stack goes here.
+  }
+}
