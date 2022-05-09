99ce38ea026e773d8a7216710b78a7fbab9127e4
Justine Breuch <justinebreuch@users.noreply.github.com>
2022-05-09 09:30:53 -0400

    Load most basic MapBox centered on continental US. This contains no data (#6)
    Very basic MapBox app with default styles. Centered on the US. Uses public API token. We need to figure out how to securely store our own API tokens.

---
+++ client/src/components/MapView.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="map-container"></div>
+</template>
+
+<script>
+import mapbox from "mapbox-gl";
+
+const DEFAULT_LATITUDE = 39.8097343;
+const DEFAULT_LONGITUDE = -98.5556199;
+
+export default {
+  name: "MapView",
+  setup() {
+    // TODO: Hide access tokens. For now, this is MapBox's public API token.
+    mapbox.accessToken = process.env.VUE_APP_MAP_BOX_API_TOKEN
+  },
+  data() {
+    return {
+      map: {}
+    }
+  },
+  props: {
+    /**
+     * Where to load the initial map. Defaults to center of continental US.
+     */
+    center: {
+      type: Array,
+      default: function () {
+        return [DEFAULT_LONGITUDE, DEFAULT_LATITUDE];
+      }
+    }
+  },
+  methods: {
+    async createMap() {
+      try {
+        this.map = new mapbox.Map({
+          // Removes watermark by Mapbox.
+          attributionControl: false,
+          center: this.center,
+          container: "map-container",
+          style: "mapbox://styles/mapbox/streets-v11",
+          zoom: 4,
+        });
+
+      } catch (err) {
+        // TODO: Add error handling.
+        console.log("Error: ", err);
+      }
+    },
+  },
+  mounted() {
+    this.createMap()
+  }
+}
+</script>
+
+<style scoped>
+  #map-container {
+    height: 100vh;
+  }
+</style>
