a983f71b5e507b0e67f005a454994b74c9a2c9c4
Brett Beekley <beekley@google.com>
2022-05-11 16:23:05 -0700

    Change naming structure.

--- cdk/lib/open-data-platform-stack.ts
+++ cdk/lib/open-data-platform-stack.ts
@@ -2,21 +2,15 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
-const appName = 'OpenDataPlatform'
-const stackName = (id: string) => {
-  return `${process.env.USER}${appName}${id}`
-}
-
 export class OpenDataPlatformStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-
+    // This is all one file right now while we initialize the stack.
     // TODO(fellows): break the stack into separate files, similar to https://github.com/BlueConduit/tributary/tree/main/cdk.
-    // This is all one file right now while we get the project initialized.
 
     // Set up networking.
-    const vpc = new ec2.Vpc(this, stackName('VPC'), {
+    const vpc = new ec2.Vpc(this, 'VPC', {
       cidr: "10.0.0.0/16",
       maxAzs: 2,
       subnetConfiguration: [
