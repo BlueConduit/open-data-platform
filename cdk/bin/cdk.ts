6d0b201159555104b68bcad31ce037a10dadcd42
Brett Beekley <beekley@google.com>
2022-05-11 16:08:29 -0700

    Add basic VPC.

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -13,9 +13,9 @@ new OpenDataPlatformStack(app, 'CdkStack', {
    * and Region that are implied by the current CLI configuration. */
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  // Development account.
+  // TODO: replace with AWSGoogleSandbox account.
+  env: { account: '036999211278', region: 'us-east-2' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
