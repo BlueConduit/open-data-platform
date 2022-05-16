dd8ad78f6793a784c09754a0904fc264538832c9
kailamjeter <kaila@blueconduit.com>
2022-05-16 16:00:59 -0400

    Switch to violations by county and add conditional styling.

--- client/src/components/MapView.vue
+++ client/src/components/MapView.vue
@@ -11,6 +11,31 @@ const DEFAULT_LONGITUDE = -98.5556199;
 
 const OPEN_DATA_PLATFORM_API_URL = 'https://v2rz6wzmb7.execute-api.us-east-2.amazonaws.com/default';
 
+// TODO(kaila): figure out useful buckets.
+const POPULATION_COLOR_MAP =
+    {
+      0:
+      '#E1F5FE',
+      10000:
+      '#B3E5FC',
+      25000:
+      '#81D4FA',
+      50000:
+      '#4FC3F7',
+      100000:
+      '#29B6F6',
+      200000:
+      '#0288D1',
+      500000:
+      '#01579B',
+      750000:
+      '#0D47A1',
+      1000000:
+      '#303F9F',
+      2000000:
+      '#1A237E'
+    };
+
 export default {
   name: "MapView",
   setup() {
@@ -34,7 +59,7 @@ export default {
     }
   },
   methods: {
-    async createMap(waterBoundaryData) {
+    async createMap(leadRuleViolationData) {
       try {
         this.map = new mapbox.Map({
           // Removes watermark by Mapbox.
@@ -46,24 +71,51 @@ export default {
         });
 
         this.map.on('load', () => {
-          this.map.addSource('water-boundaries', {
+          this.map.addSource('epa-violations', {
             type: 'geojson',
-            data: waterBoundaryData
+            data: leadRuleViolationData
           });
 
           // Add style layer for water system boundary data. Without this layer
           // water boundary data will not appear.
           this.map.addLayer({
-            'id': 'water-boundaries-style',
-            'source': 'water-boundaries',
-            'type': 'line',
+            'id': 'violations-style',
+            'source': 'epa-violations',
+            'type': 'fill',
+            'paint': {
+              'fill-color': [
+                'interpolate',
+                ['linear'],
+                ['get', 'population_served_count'],
+                0,
+                POPULATION_COLOR_MAP[0],
+                10000,
+                POPULATION_COLOR_MAP[10000],
+                25000,
+                POPULATION_COLOR_MAP[25000],
+                50000,
+                POPULATION_COLOR_MAP[50000],
+                100000,
+                POPULATION_COLOR_MAP[100000],
+                200000,
+                POPULATION_COLOR_MAP[200000],
+                500000,
+                POPULATION_COLOR_MAP[500000],
+                750000,
+                POPULATION_COLOR_MAP[750000],
+                1000000,
+                POPULATION_COLOR_MAP[0],
+                2000000,
+                POPULATION_COLOR_MAP[2000000],
+              ],
+              'fill-opacity': 0.75
+            },
             'layout': {
               // Make the layer visible by default.
               'visibility': 'visible'
             },
           });
         });
-
       } catch (err) {
         // TODO: Add error handling.
         console.log("Error: ", err);
@@ -74,7 +126,7 @@ export default {
     // Fetch initial map data from API.
     // TODO(kailajeter): Create initial load method to load in all data sources.
     axios
-        .get(`${OPEN_DATA_PLATFORM_API_URL}/getWaterSystems`)
+        .get(`${OPEN_DATA_PLATFORM_API_URL}/getViolations`)
         .then(response => this.createMap(response.data.toString()));
   }
 }
