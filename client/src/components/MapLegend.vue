adca5daf921651a882e52ceddea260ee225c17ab
kailamjeter <kaila@blueconduit.com>
2022-05-18 16:12:16 -0400

    Add popup component and attach to map via clickhandler. Add formatting and comments to legend component.

--- client/src/components/MapLegend.vue
+++ client/src/components/MapLegend.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="map-overlay" id="legend">
-    <div class="legend-title">{{ this.title }}</div><p></p>
+    <div class="legend-title">{{ this.title }}</div>
+    <p></p>
     <div class="legend-entry" v-for="(item, index) in bucketLabels" :key="item">
-      <span class="color" :style="{'background-color': bucketColors[index]}"></span>
+      <span class="color"
+            :style="{'background-color': bucketColors[index]}"></span>
       <span class="label"> {{ bucketLabels[index] }}</span>
     </div>
   </div>
 </template>
 
 <script>
+/**
+ * Map legend component.
+ *
+ * Takes title and map of string keys -> hex string color values.
+ */
 export default {
   name: "MapLegend",
   data() {
@@ -35,18 +42,15 @@ export default {
       legendKeys.forEach((legendKey, i) => {
         // If this is the last legendKey, create label which is `legendKey+`.
         // Otherwise, create label which is the range `currentKey - nextKey`.
-         this.bucketLabels.push(
-             i < legendKeys.length - 1
-             ? `${legendKey} - ${legendKeys[i + 1]}` :`${legendKey}+`);
-         this.bucketColors.push(legendValues[i]);
+        this.bucketLabels.push(
+            i < legendKeys.length - 1
+                ? `${legendKey} - ${legendKeys[i + 1]}` : `${legendKey}+`);
+        this.bucketColors.push(legendValues[i]);
       });
     }
   },
   mounted() {
-    // If initial data set when component is mounted, build legend.
-    if (this.title != null && this.bucketKeyValueMap != null) {
-      this.createLegend();
-    }
+    this.createLegend();
   },
   watch: {
     // When map is updated, rebuild legend.
@@ -58,20 +62,32 @@ export default {
 </script>
 
 <style scoped>
-  #legend {
-    padding: 10px;
-    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
-    line-height: 20px;
-  }
-  .legend-entry {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-  }
-  .color {
-    border-radius: 20%;
-    width: 10px;
-    height: 10px;
-    margin-right: 5px;
-  }
+.color {
+  border-radius: 20%;
+  width: 10px;
+  height: 10px;
+  margin-right: 5px;
+}
+
+#legend {
+  padding: 10px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+  line-height: 20px;
+}
+
+.legend-entry {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.map-overlay {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  background: #fff;
+  margin-right: 20px;
+  overflow: auto;
+  border-radius: 3px;
+}
 </style>
--- client/src/components/MapView.vue
+++ client/src/components/MapView.vue
@@ -9,40 +9,46 @@
 import mapbox from 'mapbox-gl';
 import axios from 'axios'
 import MapLegend from '@/components/MapLegend';
+import MapPopupContent from '@/components/MapPopupContent';
+import {createApp, defineComponent, nextTick} from 'vue'
 
 const DEFAULT_LATITUDE = 39.8097343;
 const DEFAULT_LONGITUDE = -98.5556199;
 
 const OPEN_DATA_PLATFORM_API_URL = 'https://v2rz6wzmb7.execute-api.us-east-2.amazonaws.com/default';
+const POPUP_CONTENT_BASE_ID = 'popup-content';
+const POPUP_CONTENT_BASE_HTML = `<div id="${POPUP_CONTENT_BASE_ID}"></div>`;
 
 // TODO(kaila): figure out useful buckets.
 const POPULATION_COLOR_MAP =
     {
       0:
-      '#E1F5FE',
+          '#E1F5FE',
       10000:
-      '#B3E5FC',
+          '#B3E5FC',
       25000:
-      '#81D4FA',
+          '#81D4FA',
       50000:
-      '#4FC3F7',
+          '#4FC3F7',
       100000:
-      '#29B6F6',
+          '#29B6F6',
       200000:
-      '#0288D1',
+          '#0288D1',
       500000:
-      '#01579B',
+          '#01579B',
       750000:
-      '#0D47A1',
+          '#0D47A1',
       1000000:
-      '#303F9F',
+          '#303F9F',
       2000000:
-      '#1A237E'
+          '#1A237E'
     };
 
 export default {
   name: "MapView",
-  components: {MapLegend},
+  components: {
+    MapLegend,
+  },
   setup() {
     // TODO: Hide access tokens. For now, this is MapBox's public API token.
     mapbox.accessToken = process.env.VUE_APP_MAP_BOX_API_TOKEN
@@ -66,6 +72,51 @@ export default {
     }
   },
   methods: {
+    /**
+     * Creates popup component at the given lngLat.
+     *
+     * Passes propsData to the MapPopupContent component which is nested in the
+     * popup.
+     */
+    createMapPopup(lngLat, popupData) {
+      new mapbox.Popup({className: 'mapbox-popup'})
+          .setLngLat(lngLat)
+          .setHTML(POPUP_CONTENT_BASE_HTML) // Add basic div to mount to.
+          .addTo(this.map);
+
+      // Creates component which extends MapPopupContent and mounts it onto the
+      // div passed to the Popup.
+      const MapPopup = defineComponent({
+        extends: MapPopupContent,
+      })
+      nextTick(() => {
+        createApp(MapPopup, popupData).mount(`#${POPUP_CONTENT_BASE_ID}`)
+      });
+    },
+
+    /**
+     * Sets up interaction handlers for map.
+     */
+    setUpInteractionHandlers() {
+      this.map.on('click', 'epa-violations-population-style', async (e) => {
+        const clickedFeature = e.features[0];
+
+        this.createMapPopup(
+            e.lngLat,
+            /* popupData= */
+            {
+              title: 'County',
+              properties: new Map(Object.entries(clickedFeature.properties))
+            });
+      });
+    },
+
+    /**
+     * Creates base Mapbox map and configures interactions and styles for
+     * layers.
+     *
+     * @param leadRuleViolationData initial data to display.
+     */
     async createMap(leadRuleViolationData) {
       try {
         this.map = new mapbox.Map({
@@ -86,7 +137,7 @@ export default {
           // Add style layer for water system boundary data. Without this layer
           // water boundary data will not appear.
           this.map.addLayer({
-            'id': 'violations-style',
+            'id': 'epa-violations-population-style',
             'source': 'epa-violations',
             'type': 'fill',
             'paint': {
@@ -127,6 +178,8 @@ export default {
           // Initial values for legend based on visible layer.
           this.legendTitle = 'Population Served';
           this.legendKeyValueMap = POPULATION_COLOR_MAP;
+
+          this.setUpInteractionHandlers();
         });
       } catch (err) {
         // TODO: Add error handling.
@@ -144,8 +197,18 @@ export default {
 }
 </script>
 
-<style scoped>
-  #map-container {
-    height: 100vh;
-  }
+<style>
+#map-container {
+  height: 100vh;
+}
+
+.mapboxgl-popup-tip {
+  visibility: hidden;
+}
+
+#popup-content {
+  border-radius: 8px;
+  width: 258px;
+  height: 256px;
+}
 </style>
