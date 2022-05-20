ff64991280bc5cd1ab2219dc01e92ebd20644b0b
Justine Breuch <breuch@google.com>
2022-05-20 15:20:55 -0400

    Update formatting on multiline comments

--- cdk/src/open-data-platform/lambda/write-demographic-data-handler.ts
+++ cdk/src/open-data-platform/lambda/write-demographic-data-handler.ts
@@ -13,68 +13,68 @@ const BLACK_POPULATION = 'Estimate!!Total:!!Black or African American alone';
 
 // Prepared SQL statements.
 const DELETE_DEMOGRAPHICS_TABLE =
-  'DELETE FROM demographics WHERE census_geo_id IS NOT NULL';
+    'DELETE FROM demographics WHERE census_geo_id IS NOT NULL';
 
 // Reads the S3 CSV file and returns [DemographicsTableRow]s.
 const parseS3IntoDemographicsTableRow =
-  (s3Params: Object): Promise<Array<DemographicsTableRow>> => {
-    const results: DemographicsTableRow[] = [];
-    return new Promise(function (resolve, reject) {
-      let count = 0;
-      const fileStream = S3.getObject(s3Params).createReadStream();
-      fileStream.pipe(parse())
-                .on('data',
-                  (chunk) => {
-                    // Pause to allow for processing.
-                    fileStream.pause();
-                    // Only process 10 rows for now.
-                    if (count < 10) {
-                      const row =
+    (s3Params: Object, numberRowsToWrite = 10): Promise<Array<DemographicsTableRow>> => {
+      const results: DemographicsTableRow[] = [];
+      return new Promise(function (resolve, reject) {
+        let count = 0;
+        const fileStream = S3.getObject(s3Params).createReadStream();
+        fileStream.pipe(parse())
+            .on('data',
+                (chunk) => {
+                  // Pause to allow for processing.
+                  fileStream.pause();
+                  // Only process 10 rows for now.
+                  if (count < numberRowsToWrite) {
+                    const row =
                         new DemographicsTableRowBuilder()
-                          .censusGeoId(chunk[GEO_ID])
-                          .totalPopulation(parseInt(chunk[RACE_TOTAL]))
-                          .blackPopulation(parseInt(chunk[BLACK_POPULATION]))
-                          .whitePopulation(parseInt(chunk[WHITE_POPULATION]))
-                          .build();
-                      results.push(row);
-                    }
-                    count += 1;
-                    fileStream.resume();
-                  })
-                .on('error',
-                  (error) => {
-                    reject(error);
-                  })
-                .on('end', () => {
-                  console.log('Parsed ' + results.length + ' rows.');
-                  resolve(results);
-                });
-    });
-  }
+                            .censusGeoId(chunk[GEO_ID])
+                            .totalPopulation(parseInt(chunk[RACE_TOTAL]))
+                            .blackPopulation(parseInt(chunk[BLACK_POPULATION]))
+                            .whitePopulation(parseInt(chunk[WHITE_POPULATION]))
+                            .build();
+                    results.push(row);
+                  }
+                  count += 1;
+                  fileStream.resume();
+                })
+            .on('error',
+                (error) => {
+                  reject(error);
+                })
+            .on('end', () => {
+              console.log('Parsed ' + results.length + ' rows.');
+              resolve(results);
+            });
+      });
+    }
 
 // Executes SQL statement argument against the MainCluster db.
 const executeSqlStatement = async (secretArn: string, resourceArn: string,
                                    statement: string,
-                                   db: String | undefined): Promise<Object> => {
+                                   db: string = 'postgres'): Promise<Object> => {
   return new Promise(async function (resolve, reject) {
     const sqlParams = {
       secretArn: secretArn,
       resourceArn: resourceArn,
       sql: statement,
-      database: db ?? 'postgres',  // Default db
+      database: db,
       includeResultMetadata: true
     };
 
     await rdsDataService.executeStatement(sqlParams,
-      function (err: Error, data: Object) {
-        if (err) {
-          console.log(err);
-          reject(err);
-        } else {
-          console.log('Data is: ' + JSON.stringify(data));
-          resolve(data);
-        }
-      });
+        function (err: Error, data: Object) {
+          if (err) {
+            console.log(err);
+            reject(err);
+          } else {
+            console.log('Data is: ' + JSON.stringify(data));
+            resolve(data);
+          }
+        });
   });
 }
 // Formats a [DemographicsTableRow] as SQL row.
@@ -86,12 +86,15 @@ const formatPopulationTableRowAsSql = (row: DemographicsTableRow): string => {
   return '(' + singleValue.join(',') + ')';
 }
 
-// Parses S3 'alabama_acs_data.csv' file and writes rows to demographics
-// table in the MainCluster postgres db.
+/*
+ * Parses S3 'alabama_acs_data.csv' file and writes rows to demographics
+ * table in the MainCluster postgres db.
+ */
 exports.handler = async (event: APIGatewayEvent): Promise<Object> => {
   return new Promise(async function (resolve, reject) {
     const secretArn = event['secretArn'];
     const mainClusterArn = event['mainClusterArn'];
+    const numberRowsToWrite = event['numberRows'];
 
     const s3Params = {
       Bucket: 'opendataplatformapistaticdata',
@@ -100,22 +103,22 @@ exports.handler = async (event: APIGatewayEvent): Promise<Object> => {
 
     // Read CSV file and write to demographics table.
     try {
-      let rows = await parseS3IntoDemographicsTableRow(s3Params);
+      let rows = await parseS3IntoDemographicsTableRow(s3Params, numberRowsToWrite);
       const valuesForSql = rows.map(formatPopulationTableRowAsSql);
 
       const insertRowsIntoDemographicsTableStatement =
-        'INSERT INTO demographics  \n' +
-        '(census_geo_id, total_population, black_percentage, white_percentage) \n' +
-        'VALUES \n' + valuesForSql.join(', ') + '; \n';
+          'INSERT INTO demographics  \n' +
+          '(census_geo_id, total_population, black_percentage, white_percentage) \n' +
+          'VALUES \n' + valuesForSql.join(', ') + '; \n';
 
       console.log(
-        'Running statement: ' +
-        insertRowsIntoDemographicsTableStatement);
+          'Running statement: ' +
+          insertRowsIntoDemographicsTableStatement);
 
       // Note: pass in DELETE_DEMOGRAPHICS_TABLE to drop rows first.
       let data = await executeSqlStatement(
-        secretArn, mainClusterArn,
-        insertRowsIntoDemographicsTableStatement, 'postgres');
+          secretArn, mainClusterArn,
+          insertRowsIntoDemographicsTableStatement, 'postgres');
 
       resolve({statusCode: 200, body: JSON.stringify(data)});
 
@@ -134,8 +137,8 @@ class DemographicsTableRow {
   whitePopulation: number;
 
   constructor(
-    censusGeoId: string, totalPopulation: number, blackPopulation: number,
-    whitePopulation: number) {
+      censusGeoId: string, totalPopulation: number, blackPopulation: number,
+      whitePopulation: number) {
     this.censusGeoId = censusGeoId;
     this.totalPopulation = totalPopulation;
     this.blackPopulation = blackPopulation;
