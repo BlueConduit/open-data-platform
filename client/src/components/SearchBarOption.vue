24aa6d055c64b6b1798d9284be052cee4334dac3
kailamjeter <kaila@blueconduit.com>
2022-05-24 09:36:26 -0400

    Searchbar which contains one option and no functionality. Initial component outlines for searchbar.

---
+++ client/src/components/SearchBarOption.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="selected" v-if="selected">
+    {{ textContent }}
+  </div>
+  <div class="unselected" v-else>
+    {{ textContent }}
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: "SearchBarOption",
+  props: {
+    textContent: {
+      type: String,
+      required: true,
+    },
+    selected: {
+      type: Boolean,
+      default: false
+    },
+  }
+})
+</script>
+
+<style scoped>
+.selected, .unselected {
+  width: 186px;
+  height: 26px;
+  border-radius: 30px;
+}
+
+.selected {
+  background-color: #E5E5E580;
+  border: 1.5px solid #000000;
+}
+
+.unselected {
+  background-color: white;
+  border: 1.5px solid grey;
+}
+</style>
