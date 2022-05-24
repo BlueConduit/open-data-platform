00d0043b1c4111f52368d3ae538c0d4ee6e6f221
kailamjeter <kaila@blueconduit.com>
2022-05-24 09:36:26 -0400

    Searchbar which contains one option and no functionality. Initial component outlines for searchbar.

--- client/src/components/MapContainer.vue
+++ client/src/components/MapContainer.vue
@@ -1,10 +1,13 @@
 <template>
+  <SearchBar :initial-selected-option="this.options[0]"
+             :options="this.options"></SearchBar>
   <MapView/>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
 import MapView from './MapView.vue'
+import SearchBar from './SearchBar.vue'
 
 /**
  * Wrapper for national map dashboard.
@@ -12,7 +15,13 @@ import MapView from './MapView.vue'
 export default defineComponent({
   name: 'MapContainer',
   components: {
-    MapView
+    MapView,
+    SearchBar,
+  },
+  data() {
+    return {
+      options: ['Population'],
+    }
   }
 })
 </script>
