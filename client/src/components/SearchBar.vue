96ccb464bb280774d63a77d12a6ce852f24e15ac
kailamjeter <kaila@blueconduit.com>
2022-05-24 09:36:26 -0400

    Searchbar which contains one option and no functionality. Initial component outlines for searchbar.

---
+++ client/src/components/SearchBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <div>
+      <search-bar-option
+          v-for="option in options"
+          :key="option"
+          :text-content="option"
+          :selected="selected(option)">
+      </search-bar-option>
+    </div>
+    <div class="right-align">
+      <img src="../assets/search-icon.png" class="icon"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from 'vue';
+import SearchBarOption from './SearchBarOption.vue';
+
+export default defineComponent({
+  name: "SearchBar",
+  components: {SearchBarOption},
+  data() {
+    return {
+      selectedOption: this.initialSelectedOption,
+    }
+  },
+  props: {
+    initialSelectedOption: {
+      type: String,
+      required: true,
+    },
+    options: {
+      type: Array as PropType<string[]>,
+      required: true,
+    }
+  },
+  methods: {
+    selected(option: string) {
+      return option === this.selectedOption;
+    }
+  },
+})
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  height: 54px;
+}
+
+.container  div {
+  padding: 0 30px;
+  display: flex;
+  align-items: center;
+}
+
+.right-align {
+  flex-grow: 1;
+  justify-content: flex-end;
+}
+</style>
