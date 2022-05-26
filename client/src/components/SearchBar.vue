387cf3576df9ffade7769f24ecc1c343d2b99b2d
kailamjeter <kaila@blueconduit.com>
2022-05-26 16:11:40 -0400

    Provide / inject data layer options into searchbar. Add state and data layer classes. Move searchbar out of map container.

--- client/src/components/SearchBar.vue
+++ client/src/components/SearchBar.vue
@@ -1,55 +1,78 @@
 <template>
-  <div class="container">
+  <div class='container'>
     <div class='data-layer-options'>
       <search-bar-option
         v-for='option in options'
         :key='option'
-        :text-content='option'
+        :text-content='option.name'
         :selected='getSelected(option)'
         @click='setSelected(option)'>
       </search-bar-option>
     </div>
-    <div class="right-align">
-      <div class="select-wrapper">
-        <vue-select v-model="this.selectedOption" :options="this.options"/>
+    <div class='right-align'>
+      <div class='select-wrapper'>
+        <vue-select label='name' v-model='this.selectedOption'
+                    :options='this.options' />
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, inject } from 'vue';
 import SearchBarOption from './SearchBarOption.vue';
 import VueSelect from 'vue-select';
 import 'vue-select/dist/vue-select.css';
+import { State } from '../model/state';
+import { stateKey } from '../injection_keys';
+import { DataLayer } from '../model/data_layer';
 
 export default defineComponent({
-  name: "SearchBar",
-  components: {SearchBarOption, VueSelect},
+  name: 'SearchBar',
+  components: { SearchBarOption, VueSelect },
+  setup() {
+    const store: State | undefined = inject(stateKey);
+
+    return {
+      store,
+    };
+  },
   data() {
     return {
-      selectedOption: this.initialSelectedOption,
-    }
-  },
-  props: {
-    initialSelectedOption: {
-      type: String,
-      required: true,
-    },
-    options: {
-      type: Array as PropType<string[]>,
-      required: true,
-    }
+      // TODO figure out how to not have a defaults
+      selectedOption: new DataLayer('fake layer'),
+      options: [] as DataLayer[],
+    };
   },
   methods: {
-    getSelected(option: string): boolean {
+
+    /**
+     * Returns whether option is the selected option.
+     *
+     * @param option
+     */
+    getSelected(option: DataLayer): boolean {
       return option === this.selectedOption;
     },
-    setSelected(option: string): void {
+
+    /**
+     * Updates the selected option.
+     * @param option
+     */
+    setSelected(option: DataLayer): void {
       this.selectedOption = option;
+      if (this.store) {
+        this.store.setCurrentDataLayer(this.selectedOption);
+      }
     },
   },
-})
+  mounted() {
+    if (this.store != null) {
+      this.options = this.store.dataLayers;
+      this.selectedOption = this.store.currentDataLayer;
+    }
+  },
+});
 </script>
 
 <style scoped>
