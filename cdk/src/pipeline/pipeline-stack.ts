1f55bf19a485e83c5752637351f7f49562183324
Brett Beekley <beekley@google.com>
2022-07-06 11:02:39 -0700

    Fix some strings.

--- cdk/src/pipeline/pipeline-stack.ts
+++ cdk/src/pipeline/pipeline-stack.ts
@@ -7,6 +7,10 @@ import * as pipelines from 'aws-cdk-lib/pipelines';
 import * as util from '../util';
 import { OpenDataPlatformStage } from './stage';
 
+const CODE_REPO = 'BlueConduit/open-data-platform';
+const CODE_CONNECTION_ARN =
+  'arn:aws:codestar-connections:us-east-2:223904267317:connection/cf8a731a-3a36-4e74-ac7f-d93604fd258e';
+
 export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -17,9 +21,8 @@ export class PipelineStack extends Stack {
       selfMutation: true,
       dockerEnabledForSynth: true,
       synth: new pipelines.ShellStep('Synth', {
-        input: pipelines.CodePipelineSource.connection('BlueConduit/open-data-platform', 'main', {
-          connectionArn:
-            'arn:aws:codestar-connections:us-east-2:223904267317:connection/cf8a731a-3a36-4e74-ac7f-d93604fd258e',
+        input: pipelines.CodePipelineSource.connection(CODE_REPO, 'main', {
+          connectionArn: CODE_CONNECTION_ARN,
         }),
         commands: [
           // Update npm.
@@ -30,15 +33,15 @@ export class PipelineStack extends Stack {
           'npm --prefix client run build',
 
           // Synthesize the CDK app.
-          'npm --prefix run cdk ci',
-          'npm --prefix run cdk pipeline-synth',
+          'npm --prefix cdk ci',
+          'npm --prefix cdk run pipeline-synth',
         ],
         primaryOutputDirectory: 'cdk/cdk.out',
       }),
     });
 
     pipeline.addStage(
-      new OpenDataPlatformStage(scope, 'Development', {
+      new OpenDataPlatformStage(scope, 'Dev', {
         env: { account: '036999211278', region: 'us-east-2' },
         tags: { Project: util.projectName, Environment: util.EnvType.Development },
         envType: util.EnvType.Development,
