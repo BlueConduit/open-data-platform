38b316c436afb0644b0439f58e96a5b6873fa6ef
Brett Beekley <beekley@google.com>
2022-07-29 09:33:59 -0700

    simple styling.

--- client/src/components/ScorecardSearch.vue
+++ client/src/components/ScorecardSearch.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="scorecard-search">
     <h1>Get Your Drinking Water Score</h1>
-    <p>See how your community has been impacted by lead service lines in your water system</p>
-    <input />
+    <p>Understand the likelihood of your home or community's exposure to lead service lines</p>
+    <!-- TODO: add ZIP, city, state as they are supported. -->
+    <input placeholder="Enter your address" />
     <button>Get Score</button>
   </div>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import { defineComponent } from 'vue';
 
 /**
@@ -18,9 +19,9 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>  
-  .scorecard-search{
-    text-align: center;
-    vertical-align: middle;
-  }
+<style scoped>
+.scorecard-search {
+  text-align: center;
+  vertical-align: middle;
+}
 </style>
