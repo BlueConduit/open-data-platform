ffaeb8cfe770c6cc87536d52e8e378e6c755ba2b
Justine Breuch <justinebreuch@users.noreply.github.com>
2022-08-01 15:07:12 -0400

    /watersystem/{pws_id} and /zipcode/scorecard/{zip_code} (#83)
    Implement lambdas and endpoints to serve zip code scorecard and water system data

--- cdk/src/open-data-platform/data-plane/data-import/utils/lambda-function-factory.ts
+++ cdk/src/open-data-platform/data-plane/data-import/utils/lambda-function-factory.ts
@@ -4,7 +4,8 @@ import * as path from 'path';
 
 import { Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { SchemaProps } from '../schema/schema-props';
+import { SchemaProps } from '../../schema/schema-props';
+import { cwd } from 'process';
 
 /**
  * Produces a lambda function that requires db access to import data.
@@ -12,13 +13,13 @@ import { SchemaProps } from '../schema/schema-props';
  * @param props: schema identifiers passed to the function when writing data.
  * @param id: ID to assign resource.
  */
-export const lambdaFactory = (
+export const dataImportLambdaFactory = (
   scope: Construct,
   props: SchemaProps,
   id: string,
 ): lambda.NodejsFunction => {
   return new lambda.NodejsFunction(scope, `${id}-handler`, {
-    entry: `${path.resolve(__dirname)}/${id}.handler.ts`,
+    entry: `${path.resolve(__dirname)}/../${id}.handler.ts`,
     handler: 'handler',
     vpc: props.vpc,
     vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_NAT },
@@ -35,3 +36,29 @@ export const lambdaFactory = (
     },
   });
 };
+
+/**
+ * Produces a lambda function that requires db access to import data.
+ * @param scope: the construct under which to provision resource.
+ * @param props: schema identifiers passed to the function when reading data.
+ * @param id: ID to assign resource.
+ */
+export const apiLambdaFactory = (
+  scope: Construct,
+  props: SchemaProps,
+  id: string,
+): lambda.NodejsFunction =>
+  new lambda.NodejsFunction(scope, `${id}-handler`, {
+    entry: `${cwd()}/../api/src/${id}/get.handler.ts`,
+    handler: 'handler',
+    vpc: props.vpc,
+    vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_NAT },
+    environment: {
+      CREDENTIALS_SECRET: props.credentialsSecret.secretArn,
+      DATABASE_NAME: props.db,
+      RESOURCE_ARN: props.cluster.clusterArn,
+    },
+    bundling: {
+      externalModules: ['aws-sdk'],
+    },
+  });
