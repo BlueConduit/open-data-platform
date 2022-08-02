7feddad152dbf722f6a1ff167386dd86d63b5595
Justine Breuch <breuch@google.com>
2022-08-02 10:45:14 -0400

    FIx weird concatenated strings

--- client/src/assets/messages/scorecard_summary_messages.ts
+++ client/src/assets/messages/scorecard_summary_messages.ts
@@ -4,12 +4,14 @@
 export class ScorecardSummaryMessages {
   static SCORECARD_SUMMARY_PANEL_HEADER = "What's in your score";
   static SCORECARD_SUMMARY_PANEL_SUBHEADER =
-    'The most relevant data used to determine ' + 'your lead score';
+    'The most relevant data used to determine your lead score';
   static HOME_AGE = 'Home age';
   static HOME_AGE_EXPLAINED =
-    'The year your home was built influences the likelihood it was constructed with lead pipes';
+    'The year your home was built influences the likelihood it was ' +
+    'constructed with lead pipes';
   static INCOME_LEVEL = 'Income level';
-  static INCOME_LEVEL_EXPLAINED = 'Income levels correlate to the likelihood of lead. Learn more.';
+  static INCOME_LEVEL_EXPLAINED =
+    'Income levels correlate to the likelihood ' + 'of lead. Learn more.';
   static SOCIAL_VULNERABILITY_INDEX = 'Social Vulnerability Index';
   static SOCIAL_VULNERABILITY_INDEX_EXPLAINED =
     'Social vulnerability refers to the potential negative effects on ' +
