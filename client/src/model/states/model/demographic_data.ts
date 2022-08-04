fc47c29c8ecc710cf3e88a48a48e88d3d1956291
Justine Breuch <breuch@google.com>
2022-08-04 16:22:49 -0400

    Add missing files

---
+++ client/src/model/states/model/demographic_data.ts
@@ -0,0 +1,9 @@
+/**
+ * Information about demographics at geo-granularity.
+ */
+export interface DemographicData {
+  geoId?: string;
+  averageHomeAge?: number;
+  averageSocialVulnerabilityIndex?: number;
+  averageIncome?: number;
+}
