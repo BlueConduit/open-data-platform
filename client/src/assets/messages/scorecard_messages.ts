e14608a51ad3414569aeb448aa09df24241ee17e
Justine Breuch <breuch@google.com>
2022-08-23 11:18:50 -0400

    Respond to feedback:

--- client/src/assets/messages/scorecard_messages.ts
+++ client/src/assets/messages/scorecard_messages.ts
@@ -40,7 +40,8 @@ export class ScorecardMessages {
   static LOWER_INCOME = 'Lower income';
   static RESEARCH_WATER_FILTERS = 'Research water filters';
   static SCORECARD_SUMMARY_PANEL_SUBHEADER =
-    'In addition to information from your water system, your score is also based on:';
+    'In addition to information from your water system, your score is also based on ' +
+    'these statistics for your zip code::';
   static SHARE_LEAD_OUT = 'Share LeadOut and help others know their status';
   static SOMEWHAT_DISADVANTAGED = 'Somewhat disadvantaged';
   static SOMEWHAT_LIKELY = 'somewhat likely';
