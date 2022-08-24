99fdae97c41a8f44b752ae870ca2a2e549313d11
Brett Beekley <beekley@google.com>
2022-08-24 14:51:06 -0700

    Use URL variable.

--- client/src/util/build_id.ts
+++ client/src/util/build_id.ts
@@ -1,5 +1,3 @@
-const commitUrl = 'https://github.com/BlueConduit/open-data-platform/commit';
-
 /**
  * Logs information about how this client code was built.
  */
@@ -9,8 +7,9 @@ export const logBuildInfo = () => {
   // Log build commit ID.
   // Other variables can be found at: https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-env-vars.html
   console.log(process.env);
-  if (process.env.CODEBUILD_RESOLVED_SOURCE_VERSION)
+  if (process.env.CODEBUILD_SOURCE_REPO_URL)
     console.log(
-      `Client built by CodeBuild from Git commit: ${commitUrl}/${process.env.CODEBUILD_RESOLVED_SOURCE_VERSION}`,
+      'Client built by CodeBuild from Git commit:',
+      process.env.CODEBUILD_SOURCE_REPO_URL,
     );
 };
