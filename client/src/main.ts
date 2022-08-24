4c14af51618dd8c5851046275a8092a519b8ea66
Brett Beekley <beekley@google.com>
2022-08-24 14:51:48 -0700

    Revert "Log build info."
    This reverts commit 70bdcb6378b6b0ab13de9424afbde23db2544c3c.

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,10 +3,8 @@ import App from './App.vue';
 import { router } from './router';
 import { createRedux } from '@/model/store_plugin';
 import { store } from '@/model/store';
-import { logBuildInfo } from './util/build_id';
 
 /**
  * Starts up the application.
  */
 createApp(App).use(router).use(createRedux(store)).mount('#app');
-logBuildInfo();
