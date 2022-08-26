d8cbd911763f871434cb53e3a95b9e4d286e46dc
kailamjeter <104104998+kailamjeter@users.noreply.github.com>
2022-08-26 10:27:06 -0400

    Lslr section (#133)
    * Add section without actual links.
    
    * Add city toggle.
    
    * Component working with hard coded city passed in via ScorecardView
    
    * API + insert lambda changes to test.
    
    * Geotype populated in url from landing page, temp BE fixes for deployment.
    
    * Push most recent changes. Dynamic city name not working.
    
    * Working
    
    * Small fixes.
    
    * Add comment.

--- client/src/views/ScorecardView.vue
+++ client/src/views/ScorecardView.vue
@@ -7,12 +7,14 @@
         {{ ScorecardMessages.TAKE_ACTION_HEADER }}
       </div>
       <ActionSection
+        class='section'
         :header='ScorecardMessages.ADDITIONAL_STEPS_HEADER'
         :subheader='ScorecardMessages.ADDITIONAL_STEPS_SUBHEADER'
         :buttonText='ScorecardMessages.RESEARCH_WATER_FILTERS'
         @onButtonClick='navigateToResourcePage'
       />
       <ActionSection
+        class='section'
         :header='ScorecardMessages.SHARE_LEAD_OUT'
         :buttonText='ScorecardMessages.COPY_TO_CLIPBOARD'
         @onButtonClick='copyToClipboard'
@@ -20,15 +22,17 @@
     </div>
     <ScorecardSummaryPanel />
     <ActionSection
+      class='nav-to-map section'
       :header='ScorecardMessages.WANT_TO_KNOW_MORE'
       :subheader='ScorecardMessages.EXPLORE_MAP_PAGE_EXPLAINER'
       :buttonText='Titles.EXPLORE_NATION_WIDE_MAP'
       @onButtonClick='navigateToMapPage'
     />
+    <LslrSection v-if='showLslr' :city='leadDataState?.data?.city' />
   </div>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import PredictionPanel from '../components/PredictionPanel.vue';
 import ActionSection from '../components/ActionSection.vue';
 import { defineComponent } from 'vue';
@@ -37,6 +41,11 @@ import ScorecardSummaryPanel from '../components/ScorecardSummaryPanel.vue';
 import { ScorecardMessages } from '../assets/messages/scorecard_messages';
 import { Titles } from '../assets/messages/common';
 import NationwideMap from '../components/NationwideMap.vue';
+import LslrSection from '@/components/LslrSection.vue';
+import { LSLR_CITY_LINKS } from '@/components/LslrSection.vue';
+import { useSelector } from '@/model/store';
+import { LeadDataState } from '../model/states/lead_data_state';
+import { City } from '../model/states/model/geo_data';
 
 /**
  * Container for SearchBar and MapContainer.
@@ -48,11 +57,20 @@ export default defineComponent({
     NationwideMap,
     PredictionPanel,
     ScorecardSummaryPanel,
+    LslrSection,
+  },
+  setup() {
+    const leadDataState = useSelector((state) => state.leadData) as LeadDataState;
+
+    return {
+      leadDataState,
+    };
   },
   data() {
     return {
       ScorecardMessages,
       Titles,
+      showLslr: false,
     };
   },
   methods: {
@@ -71,14 +89,24 @@ export default defineComponent({
       });
     },
   },
+  watch: {
+    'leadDataState.data.city': function() {
+      const city = this.leadDataState?.data?.city ?? City.unknown;
+      this.showLslr = city != null && LSLR_CITY_LINKS.get(city) != null;
+    },
+  },
 });
 </script>
 
-<style scoped lang="scss">
+<style scoped lang='scss'>
 @import '../assets/styles/global.scss';
 @import '@blueconduit/copper/scss/01_settings/design-tokens';
 
 .actions-to-take {
   padding: $spacing-lg;
+}
+
+.nav-to-map {
+  background-color: $light-blue-50;
 }
 </style>
