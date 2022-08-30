a1d90e7bbce4d75c8116ef7e1961b7a2440855b5
Brett Beekley <beekley@google.com>
2022-08-30 10:51:51 -0700

    Create tje Topic object.

--- cdk/src/open-data-platform/app-plane/app-plane-stack.ts
+++ cdk/src/open-data-platform/app-plane/app-plane-stack.ts
@@ -7,14 +7,13 @@ import { DataPlaneStack } from '../data-plane/data-plane-stack';
 import { NetworkStack } from '../network/network-stack';
 import { TileServer } from './tileserver/tileserver';
 import { ApiStack } from './api/api-stack';
-import { ITopic } from 'aws-cdk-lib/aws-sns';
+import { Topic } from 'aws-cdk-lib/aws-sns';
 
 // TODO: consider narrowing the props down to the specific things that the App Plane needs.
 // E.g. just the cluster object itself, rather than the entire network stack.
 export interface AppPlaneStackProps extends CommonProps {
   networkStack: NetworkStack;
   dataPlaneStack: DataPlaneStack;
-  ticketSNSTopic?: ITopic;
 }
 
 export class AppPlaneStack extends Stack {
@@ -24,7 +23,7 @@ export class AppPlaneStack extends Stack {
   constructor(scope: Construct, id: string, props: AppPlaneStackProps) {
     super(scope, id, props);
 
-    const { networkStack, dataPlaneStack } = props;
+    const { networkStack, dataPlaneStack, ticketSNSTopicArn } = props;
 
     this.tileServer = new TileServer(this, 'TileServer', {
       ecsCluster: networkStack.cluster,
@@ -32,6 +31,11 @@ export class AppPlaneStack extends Stack {
       databaseUrlSecret: dataPlaneStack.tileserverCredentials.databaseUrlSecret,
     });
 
-    this.api = new ApiStack(this, 'ApiStack', props);
+    this.api = new ApiStack(this, 'ApiStack', {
+      ...props,
+      ticketSNSTopic: ticketSNSTopicArn
+        ? Topic.fromTopicArn(this, 'ticketSNSTopic', ticketSNSTopicArn)
+        : undefined,
+    });
   }
 }
