702d7777413124067bc6cb0a7d098ee6eb22717e
Brett Beekley <beekley@google.com>
2022-09-06 11:05:21 -0700

    Make the landing page responsive via a layout-only CSS library (#153)
    * Use bulma to make a responsive footer.
    
    * Adjust BC logo to 3:1 aspect ratio.
    
    * Make resources section responsive.
    
    * Adjust links
    
    * Make search section responsive.
    
    * Make filters section responsive.
    
    * Make map section responsive.
    
    * Make navbar responsive.
    
    * Remove unused styles.
    
    * Keep mixins DRY.
    
    * Move mixins up to highest-level div.

--- client/src/components/landing_page/FilterInfoSection.vue
+++ client/src/components/landing_page/FilterInfoSection.vue
@@ -1,26 +1,30 @@
 <template>
-  <div class='section'>
-    <div class='header-section'>
-      <div class='h2-header-large'>
-        {{ messages.CHOOSING_FILTER_SECTION_SUPER_HEADER }}
-      </div>
-      <div class='h1-header-large'>
-        {{ messages.CHOOSING_FILTER_SECTION_HEADER }}
-      </div>
-      <div class='h2-header-large'>
-        {{ messages.CHOOSING_FILTER_SECTION_SUBHEADER }}
+  <div class='section is-small is-centered has-text-centered'>
+    <div class='columns'>
+      <div class='column is-two-thirds'>
+        <div class='header-section'>
+          <div class='h2-header-large'>
+            {{ messages.CHOOSING_FILTER_SECTION_SUPER_HEADER }}
+          </div>
+          <div class='h1-header-large'>
+            {{ messages.CHOOSING_FILTER_SECTION_HEADER }}
+          </div>
+          <div class='h2-header-large'>
+            {{ messages.CHOOSING_FILTER_SECTION_SUBHEADER }}
+          </div>
+        </div>
+        <button class='grey-button' @click='navigateToResources'>
+          {{ messages.LEARN_MORE_BUTTON_TEXT }}
+        </button>
       </div>
     </div>
-    <button class='grey-button' @click='navigateToResources'>
-      {{ messages.LEARN_MORE_BUTTON_TEXT }}
-    </button>
   </div>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import { defineComponent } from 'vue';
-import { LandingPageMessages as messages } from '@/assets/messages/landing';
-import { router } from '@/router';
+import { LandingPageMessages as messages } from '../../assets/messages/landing';
+import { router } from '../../router';
 
 /**
  * Content for filter info section of the landing page.
@@ -42,18 +46,19 @@ export default defineComponent({
 });
 </script>
 
-<style scoped lang='scss'>
+<style scoped lang="scss">
 @import '../../assets/styles/global.scss';
 @import '@blueconduit/copper/scss/01_settings/design-tokens';
-
-.h2-header-large {
-  @include centered-text;
-  
-  max-width: 70%;
-}
+@import 'bulma/sass/layout/section.sass';
+@import 'bulma/sass/grid/columns.sass';
+@import 'bulma/sass/helpers/typography.sass';
 
 .section {
   background-color: $light-blue;
   color: $white;
+}
+
+.h2-header-large {
+  margin-bottom: $spacing-lg;
 }
 </style>
