ce31c22877814382c0e610ba5189d6e0c7f106c2
Brett Beekley <beekley@google.com>
2022-09-07 14:28:14 -0700

    Make prediction panel responsive.

--- client/src/components/PredictionPanel.vue
+++ client/src/components/PredictionPanel.vue
@@ -1,39 +1,32 @@
 <template>
-  <div class='container'>
-    <div class='prediction'>
-      <div v-if='showWaterSystemPrediction'>
+  <div class='section is-medium'>
+    <div class='container has-text-centered'>
+      <div>
         <div class='h1-header-xl navy'>
-          {{ formatPredictionAsLikelihood(percentLead) }}
+          {{
+            (() => {
+              if (showWaterSystemPrediction) return formatPredictionAsLikelihood(percentLead);
+              if (showParcelPrediction) return formatPredictionAsLikelihood(publicLeadPercent);
+              if (showNoPrediction) return ScorecardSummaryMessages.NOT_ENOUGH_DATA_AVAILABLE;
+              return ScorecardSummaryMessages.GET_WATER_SCORE;
+            })()
+          }}
         </div>
         <div class='h2-header'>
-          {{ ScorecardSummaryMessages.PREDICTION_EXPLANATION(
-          formatPredictionAsLikelihoodDescriptor(percentLead)) }}
-        </div>
-      </div>
-      <div class='h1-header'
-           v-if='showParcelPrediction'>
-        <div class='h1-header-xl navy'>
-          {{ formatPredictionAsLikelihood(publicLeadPercent) }}
-        </div>
-        <div class='h2-header'>
-          {{ ScorecardSummaryMessages.PREDICTION_EXPLANATION(
-          formatPredictionAsLikelihoodDescriptor(publicLeadPercent)) }}
-        </div>
-      </div>
-      <div class='no-prediction' v-if='showNoPrediction'>
-        <div class='h1-header-xl navy'>
-          {{ ScorecardSummaryMessages.NOT_ENOUGH_DATA_AVAILABLE }}
-        </div>
-        <div class='explain-text'>
-          {{ ScorecardSummaryMessages.NOT_ENOUGH_DATA_EXPLAINED }}
-        </div>
-      </div>
-      <div v-if='emptyGeoData'>
-        <div class='h1-header-large navy'>
-          {{ ScorecardSummaryMessages.GET_WATER_SCORE }}
-        </div>
-        <div class='explain-text'>
-          {{ ScorecardSummaryMessages.LEAD_LIKELIHOOD_EXPLAINED }}
+          {{
+            (() => {
+              if (showWaterSystemPrediction)
+                return ScorecardSummaryMessages.PREDICTION_EXPLANATION(
+                  formatPredictionAsLikelihoodDescriptor(percentLead),
+                );
+              if (showParcelPrediction)
+                return ScorecardSummaryMessages.PREDICTION_EXPLANATION(
+                  formatPredictionAsLikelihoodDescriptor(publicLeadPercent),
+                );
+              if (showNoPrediction) return ScorecardSummaryMessages.NOT_ENOUGH_DATA_EXPLAINED;
+              return ScorecardSummaryMessages.LEAD_LIKELIHOOD_EXPLAINED;
+            })()
+          }}
         </div>
       </div>
       <!--      TODO: show error message when content is finalized and showError is true.-->
@@ -41,7 +34,7 @@
   </div>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import { defineComponent } from 'vue';
 import { dispatch, useSelector } from '../model/store';
 import { ScorecardMessages } from '../assets/messages/scorecard_messages';
@@ -97,9 +90,7 @@ export default defineComponent({
     showWaterSystemPrediction(): boolean {
       return (
         // Show water system prediction if it has a value and there is no parcel prediction.
-        !this.showParcelPrediction &&
-        this.pwsId != null &&
-        this.percentLead != null
+        !this.showParcelPrediction && this.pwsId != null && this.percentLead != null
       );
     },
     showPrediction(): boolean {
@@ -114,15 +105,15 @@ export default defineComponent({
   watch: {
     // Listen for changes to pws id or lat, long. Once it changes, a new
     // prediction must be fetched.
-    'geoState.geoids': function() {
+    'geoState.geoids': function () {
       dispatch(clearLeadData());
 
       // Check if an address was queried and another prediction should be
       // fetched.
       if (
-        this.geoState?.geoids?.address != null
-        && this.geoState?.geoids?.lat != null
-        && this.geoState?.geoids?.long != null
+        this.geoState?.geoids?.address != null &&
+        this.geoState?.geoids?.lat != null &&
+        this.geoState?.geoids?.long != null
       ) {
         dispatch(getParcel(this.geoState.geoids.lat, this.geoState.geoids.long));
       }
@@ -131,7 +122,7 @@ export default defineComponent({
         dispatch(getWaterSystem(this.geoState.geoids.pwsId.id));
       }
     },
-    'geoState.status': function() {
+    'geoState.status': function () {
       this.showError = this.geoState?.status?.status == Status.error;
     },
   },
@@ -160,25 +151,10 @@ export default defineComponent({
 });
 </script>
 
-<style scoped lang='scss'>
+<style scoped lang="scss">
 @import '../assets/styles/global.scss';
 @import '@blueconduit/copper/scss/01_settings/design-tokens';
-
-.prediction div {
-  @include container-column;
-  @include center-container;
-}
-
-.container {
-  padding: $spacing-lg;
-}
-
-.center-container {
-  gap: $spacing-lg;
-  padding: 0 3*$spacing-lg 0 3*$spacing-lg;
-}
-
-.justify-right {
-  justify-content: right;
-}
+@import 'bulma/sass/layout/section.sass';
+@import 'bulma/sass/elements/container.sass';
+@import 'bulma/sass/helpers/typography.sass';
 </style>
