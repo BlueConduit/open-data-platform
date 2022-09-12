87202877551fd634b143b5aba6ceb3d687ba734e
Justine Breuch <justinebreuch@users.noreply.github.com>
2022-09-12 14:41:45 -0400

    Allow for nulls in water systems and parcel tables (#161)
    Allow for nulls in water systems and parcel tables

--- cdk/src/open-data-platform/data-plane/model/water-systems-table.ts
+++ cdk/src/open-data-platform/data-plane/model/water-systems-table.ts
@@ -10,18 +10,22 @@ class WaterSystemsTableRow {
   pws_id: string;
   // Water system name.
   pws_name: string;
+  // Whether or not BC estimated the lead and service connections count.
+  // If false, these were reported values.
+  is_estimated: boolean;
   // Reported estimate of number of lead pipes in the boundary.
-  lead_connections_count: number;
+  lead_connections_count?: number;
   // Reported or estimated total number of connections in the boundary.
-  service_connections_count: number;
+  service_connections_count?: number;
   // Number of people served by the water system.
-  population_served: number;
+  population_served?: number;
   // GeoJSON representation of the boundaries.
   geom: string;
 
   constructor(
     pws_id: string,
     pws_name: string,
+    is_estimated: boolean,
     lead_connections_count: number,
     service_connections_count: number,
     population_served: number,
@@ -29,6 +33,7 @@ class WaterSystemsTableRow {
   ) {
     this.pws_id = pws_id;
     this.pws_name = pws_name;
+    this.is_estimated = is_estimated;
     this.lead_connections_count = lead_connections_count;
     this.service_connections_count = service_connections_count;
     this.population_served = population_served;
@@ -43,7 +48,7 @@ export class WaterSystemsTableRowBuilder {
   private readonly _row: WaterSystemsTableRow;
 
   constructor() {
-    this._row = new WaterSystemsTableRow('', '', 0, 0, 0, '');
+    this._row = new WaterSystemsTableRow('', '', true, 0, 0, 0, '');
   }
 
   pwsId(pwsId: string): WaterSystemsTableRowBuilder {
@@ -56,22 +61,27 @@ export class WaterSystemsTableRowBuilder {
     return this;
   }
 
+  isEstimated(isEstimated: boolean): WaterSystemsTableRowBuilder {
+    this._row.is_estimated = isEstimated;
+    return this;
+  }
+
   geom(geom: string): WaterSystemsTableRowBuilder {
     this._row.geom = geom;
     return this;
   }
 
-  leadConnectionsCount(leadConnectionsCount: number): WaterSystemsTableRowBuilder {
+  leadConnectionsCount(leadConnectionsCount?: number): WaterSystemsTableRowBuilder {
     this._row.lead_connections_count = leadConnectionsCount;
     return this;
   }
 
-  serviceConnectionsCount(serviceConnectionsCount: number): WaterSystemsTableRowBuilder {
+  serviceConnectionsCount(serviceConnectionsCount?: number): WaterSystemsTableRowBuilder {
     this._row.service_connections_count = serviceConnectionsCount;
     return this;
   }
 
-  populationServed(populationServed: number): WaterSystemsTableRowBuilder {
+  populationServed(populationServed?: number): WaterSystemsTableRowBuilder {
     this._row.population_served = populationServed;
     return this;
   }
@@ -87,16 +97,29 @@ export class WaterSystemsTableRowBuilder {
         value: { stringValue: this._row.pws_name },
       },
       {
+        name: 'is_estimated',
+        value: { booleanValue: this._row.is_estimated },
+      },
+      {
         name: 'lead_connections_count',
-        value: { doubleValue: this._row.lead_connections_count },
+        value: {
+          doubleValue: this._row.lead_connections_count,
+          isNull: this._row.lead_connections_count == null,
+        },
       },
       {
         name: 'service_connections_count',
-        value: { doubleValue: this._row.service_connections_count },
+        value: {
+          doubleValue: this._row.service_connections_count,
+          isNull: this._row.service_connections_count == null,
+        },
       },
       {
         name: 'population_served',
-        value: { doubleValue: this._row.population_served },
+        value: {
+          doubleValue: this._row.population_served,
+          isNull: this._row.population_served == null,
+        },
       },
       {
         name: 'geom',
