f7a39e1ca27ac4d34a699e8fee24a620be5267b3
Brett Beekley <beekley@google.com>
2022-09-19 10:42:01 -0700

    Add Prod stage to pipeline. (#171)
    * Add Prod stage to pipeline.
    
    * Limit release to once/day.
    
    * Use prod AWS account.

--- cdk/src/pipeline/pipeline-stack.ts
+++ cdk/src/pipeline/pipeline-stack.ts
@@ -8,6 +8,8 @@ import * as util from '../util';
 import { MonitoringStage, OpenDataPlatformStage } from './stage';
 import { BuildSpec, LinuxBuildImage } from 'aws-cdk-lib/aws-codebuild';
 
+const PROD_BAKE_DURATION_SECS = 24 * 60 * 60; // 1 day.
+const PROD_RELEASE_TIME = '09:00'; // 9 am local (Eastern) time.
 const CODE_REPO = 'BlueConduit/open-data-platform';
 const CODE_CONNECTION_ARN =
   'arn:aws:codestar-connections:us-east-2:223904267317:connection/cf8a731a-3a36-4e74-ac7f-d93604fd258e';
@@ -91,6 +93,49 @@ export class PipelineStack extends Stack {
         tags: { Project: util.projectName, Environment: util.EnvType.Development },
         envType: util.EnvType.Development,
       }),
+    );
+
+    pipeline.addStage(
+      new MonitoringStage(this, 'Prod-Monitoring', {
+        env: { account: '530942487205', region: 'us-east-2' },
+        tags: { Project: util.projectName, Environment: util.EnvType.Production },
+        envType: util.EnvType.Production,
+        slackConfig: {
+          slackChannelConfigurationName: `${util.EnvType.Production}-${util.projectName}-channel-config`,
+          slackWorkspaceId: 'TJTFN34NM',
+          // #leadout-prod-notifications
+          slackChannelId: 'C0426AYGBV3',
+        },
+      }),
+    );
+
+    /* TODO: Consider splitting prod out into its own pipeline.
+     *
+     * The prod deployment runs on a different schedule than dev [1]. We chose to use one pipeline
+     * for both environments for implementation/ops simplicity. However, this doesn't actually
+     * implement a "nightly" release since multiple prod releases could happen within a given day.
+     *
+     * [1] https://docs.google.com/document/d/1zZxCoXx5JzLXTOGVdvC4s8H-r82DFjfmNU-dmFPAQVI/edit#heading=h.bsmnc9iehgn
+     */
+    pipeline.addStage(
+      new OpenDataPlatformStage(this, 'Prod', {
+        env: { account: '530942487205', region: 'us-east-2' },
+        tags: { Project: util.projectName, Environment: util.EnvType.Production },
+        envType: util.EnvType.Production,
+      }),
+      {
+        // Bake the release in dev before deploying to prod, to catch any problems early.
+        pre: [
+          new pipelines.ShellStep('BakeStep', {
+            commands: [
+              // Bake a minimum of this duration.
+              `sleep ${PROD_BAKE_DURATION_SECS}`,
+              // Then wait until a specific (local) time on a weekday, to limit release to once/day.
+              `while [ $(date +%H:%M) != "${PROD_RELEASE_TIME}" -o $(date +%u) -gt 5 ]; do sleep 1; done`,
+            ],
+          }),
+        ],
+      },
     );
   }
 }
