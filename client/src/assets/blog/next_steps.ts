6e6dbeeef97af43406a1fca488b44919a5c3bcb0
Justine Breuch <breuch@google.com>
2022-10-04 12:03:31 -0400

    Add assets

---
+++ client/src/assets/blog/next_steps.ts
@@ -0,0 +1,50 @@
+export class NextSteps {
+  static title = 'You’re at risk for lead in your water. What now?';
+  static image = 'next-steps.jpg';
+  static content = `  <p>Learning that your area is at medium or high risk for lead may sound
+    concerning, however, there are a number of steps you can take immediately to
+    protect yourself to feel secure in your home once again. </p>
+  <p>
+    The first thing you should know is that testing your water for lead is the
+    only way to know if your water is truly lead-free. Some areas offer free or
+    subsidized <a
+    href='https://www.epa.gov/sites/default/files/2015-11/documents/2005_09_14_faq_fs_homewatertesting.pdf'>
+    water testing </a> from their city or local water utility, and other areas
+    may charge a fee. To find out the situation in your neighborhood, call your
+    local water utility service. Your provider may be listed in your status on
+    LeadOut or can usually be found with a quick Google search. Once you
+    determine if there are lead pipes leading into your home, your water utility
+    or a licensed plumber can help you determine options for getting pipes
+    replaced. Depending on what part of the piping contains lead, some areas
+    have free replacement programs. Look on your city’s website or call their
+    help line to learn about your options. </p>
+  <p>Whether you confirm for sure that your water contains lead or you
+    just want to play it safe, you can drink bottled water, which is a less
+    cost-effective solution over time, or use a water filter. Using a
+    lead-removing water filter is the best way to know you are drinking clean,
+    safe water from your tap. There are many different kinds of filters at
+    different price ranges. Learn more about the best kind of filter for your
+    situation here, and also ask your city or local water utility if there are
+    any free filter programs in your area. </p>
+  <p>While using a water filter or having your pipes replaced are the only true
+    solutions to remove lead from your water, there are a few actions you can
+    take <a
+      href='https://www.epa.gov/ground-water-and-drinking-water/basic-information-about-lead-drinking-water#:~:text=drinking%20water.-,Important%20Steps%20You%20Can%20Take%20to%20Reduce%20Lead%20in%20Drinking%20Water,-Have%20your%20water'>
+      according to the EPA</a> that may help reduce your lead exposure in the
+    short term. </p>
+  <p> Running your taps, or “flushing” your water system is a short-term
+    option. The evidence on flushing is mixed, but longer flushes have the
+    potential to reduce exposure to water that has been in contact with lead
+    pipes overnight. Contact your water utility to learn more about flushing
+    times in your area. Second, use cold water for drinking, cooking, and making
+    baby formula, as higher temperature water may contain more lead. Boiling
+    water does not remove lead, and may make the lead concentration higher!
+    Remember, you cannot see, taste, or smell lead dissolved in water. Learn
+    more about common lead myths here. Finally, you can clean your faucet’s
+    screen (also called an aerator). Lead particles can collect in the screen
+    and contaminate your water.
+  </p>
+  <p>Remember, if you suspect or find out you have lead in your water supply,
+    your best bet until pipes can be replaced is to invest in a filter. This
+    simple step can bring back your confidence in your drinking water.</p>`;
+}
