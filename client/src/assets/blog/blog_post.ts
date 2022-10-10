4f2dc6b26407091ddf8b3793850359f1b5b09066
Justine Breuch <justinebreuch@users.noreply.github.com>
2022-10-10 11:17:31 -0400

    History of lead + understanding lead status blog posts (#221)
    History of lead blog + understanding lead blog posts

---
+++ client/src/assets/blog/blog_post.ts
@@ -0,0 +1,9 @@
+// Minimum content for rendering a post.
+export interface BlogPost {
+  // Title of post.
+  title: string;
+  // Primary image under the header.
+  image: string;
+  // HTML of post, including links, images, etc.
+  content: string;
+}
