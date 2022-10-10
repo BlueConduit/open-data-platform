f75316b7459b2e6016c1bdbdd50edca32f4fb507
Justine Breuch <breuch@google.com>
2022-10-10 11:12:02 -0400

    Add missing posts

---
+++ client/src/assets/blog/understand_lead_status.ts
@@ -0,0 +1,54 @@
+import { BlogPost } from '@/assets/blog/blog_post';
+
+export const understandLead: BlogPost = {
+  title: 'Understanding Your Lead Status\n',
+  image: 'understand-lead.png',
+  content: `<p>So you’ve just checked your area’s lead water pipe contamination risk
+    level? Good! You can now take action to reduce your risk of lead-based
+    health impacts. Don’t see your area listed? Sign up for our mailing list to
+    get notified when new areas are added. </p>
+  <p>You might be wondering how LeadOut determines your area’s risk status.
+    There’s actually a lot that goes into it. To create the tool, water
+    analytics company BlueConduit teamed up with fellows from Google.org to
+    intake and process a lot of data, calculating a risk statuses for certain
+    areas. The things the team looked at were home age, a measurement called the
+    Area Deprivation Index(ADI), and income level. Keep reading for an
+    explanation of how all of these relate to your lead risk status.</p>
+  <div class='h2-header'>Average Home Age</div>
+  <p>While there’s no way to know for sure if you have lead pipes without
+    <a
+      href='https://www.epa.gov/sdwa/use-lead-free-pipes-fittings-fixtures-solder-and-flux-drinking-water#:~:text=In%201986%20Congress%20Amended%20the,providing%20water%20for%20human%20consumption'>
+      examining</a>
+    them (and there are two areas – pipes that go directly into your
+    home and also pipes in the street!), home age can help determine your risk
+    status for lead, because older homes are at a higher risk for lead pipes. If
+    your home’s original plumbing was built before 1986 (when lead in the US was
+    <a
+      href='https://www.epa.gov/sdwa/use-lead-free-pipes-fittings-fixtures-solder-and-flux-drinking-water#:~:text=In%201986%20Congress%20Amended%20the,providing%20water%20for%20human%20consumption.'>
+      banned*</a>
+    from plumbing use), the pipes or the shoulders connecting them are
+    more likely than newer plumbing to be made of lead. Keep in mind, it’s not
+    always just the pipes that affect your water supply – lead could also be
+    hiding in your faucets and other fixtures. </p>
+  <div class='h2-header'>Area Deprivation Index (ADI)</div>
+  <p>This index is a number based on <a
+    href='https://www.neighborhoodatlas.medicine.wisc.edu/'>17 categories</a>
+    that are used to describe socioeconomic disadvantage based on income,
+    education, household characteristics, and housing created by the Health
+    Resources and Services Administration (HRSA). ADIs are often used to
+    determine risks for your health caused by your environment. </p>
+  <div class='h2-header'>Income Level</div>
+  <p>Income levels are also a determining factor in lead risk.The income level
+    for your area is based upon census data and does not reflect each
+    household’s actual income. Because homes in lower-income areas are likely to
+    be older and less likely to have been renovated, lead risk increases in
+    areas where income is lower.
+  </p>
+  <p>Lead risk is a tricky number to pin down and doesn’t determine if you
+    absolutely do or do not have lead pipes, however, those in a high risk
+    status should consider getting their water tested by a professional or
+    buying a filter and should be on the lookout for lead poisoning symptoms. A
+    good first step is calling your water utility to learn more about the
+    options available to you.
+  </p>`,
+};
