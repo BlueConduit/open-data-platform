3f8a993c839a7fb2139fca5f1797eccbee15bb1a
Justine Breuch <breuch@google.com>
2022-10-10 10:15:58 -0400

    Undo blog view component

--- client/src/views/BlogView.vue
+++ client/src/views/BlogView.vue
@@ -7,46 +7,6 @@
     </div>
     <div class='section is-flex' v-html='content'></div>
   </div>
-  <p>You may have heard that lead exposure is dangerous, so how did it end up
-    in our water systems? </p>
-  <p>Lead is a toxic metal that is harmful to human health, but it wasn’t always
-    seen as dangerous. In the late 1800s and early 1900s, before the dangers of
-    lead were fully documented,
-    <a
-      href='https://www.epa.gov/ground-water-and-drinking-water/basic-information-about-lead-drinking-water'>
-      many cities in the USA installed lead water
-      pipes </a>.
-    In some cities, use of lead water pipes was even required by the
-    local government due to the material’s durability. Because lead water pipes
-    can last up to a century, many cities today still have their original pipes
-    in place. Today, lead is recognized as a significant health risk. There is
-    no concentration of lead in the human body that is considered “safe” as it
-    is known to cause neurological damage as well as varying other dangerous
-    health outcomes. </p>
-  <p>The issue of lead in drinking water is widespread. Lead service lines are
-    likely in use in every US state. The US Environmental Protection Agency
-    (EPA) has estimated that there are currently
-    <a
-      href='https://www.epa.gov/ground-water-and-drinking-water/basic-information-about-lead-drinking-water'>6
-      to 10 million</a> lead service lines across the country.</p>
-  <p>
-    One of the biggest challenges with finding and replacing lead water pipes is
-    that it’s difficult to know exactly which pipes contain lead. Before
-    electronic records, cities recorded information about their water systems in
-    varying levels of detail. Records of pipe locations and materials used in
-    the last century are often inaccurate, lack description, or have even been
-    lost over time. Because the information isn’t readily accessible,
-    BlueConduit works hard to make this information accessible to water
-    utilities and to the general public through LeadOut. Through predictive
-    modeling and persevering on the quest for more data, they’re able to add
-    information to LeadOut over time.
-  </p>
-  <p>Once found, replacing lead pipes is generally expensive and time consuming.
-    While many cities and water systems are working to remove known lead from
-    their infrastructure, LeadOut helps affected communities take action for
-    their health. The first steps are easy - calling your local utility company
-    to learn about water testing, ordering a filter, and educating yourself
-    about lead poisoning symptoms.</p>
 </template>
 
 <script lang='ts'>
