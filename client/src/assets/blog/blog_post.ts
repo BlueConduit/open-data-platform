f2608ab6bad72c54cf3339a7d49861eb02729e27
kailamjeter <104104998+kailamjeter@users.noreply.github.com>
2022-10-12 10:20:08 -0400

    Fix about us mobile view. (#222)
    * Fix about us mobile view.
    
    * Update message and styling a bit
    
    * Modify content and styling a bit
    
    * Fix tests.

--- client/src/assets/blog/blog_post.ts
+++ client/src/assets/blog/blog_post.ts
@@ -3,7 +3,7 @@ export interface BlogPost {
   // Title of post.
   title: string;
   // Primary image under the header.
-  image: string;
+  image?: string;
   // HTML of post, including links, images, etc.
   content: string;
 }
