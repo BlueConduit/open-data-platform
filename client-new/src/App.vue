e6907a229fc4fce430244c83a7132d24c9a6e3cf
Selena Strain <selena.strain@gmail.com>
2023-01-11 17:11:18 -0500

    Initial files for the new Vue frontend app

---
+++ client-new/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+	<main>
+		<router-view />
+	</main>
+</template>
---
+++ client-new/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import MapSection from '../components/MapSection.vue'
+</script>
+
+<template>
+	<main>
+		<MapSection />
+	</main>
+</template>
