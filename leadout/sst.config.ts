e61d0f767585b51cd7813c183a0a04772ad20c39
Aaron Beckett <rockettman.labs@gmail.com>
2023-02-12 12:39:08 -0700

    devops: prepare `leadout` for BC dev loop and ci/cd
    - set default region to `us-east-2`
    - specify named profile `dev` for local commands and debugging
    - add top level `test` command
    - add `diff` command
    - add `console` commands for staging/prod environments
    - upgrade to @tsconfig/node18
    - fix `typecheck` command

--- leadout/sst.config.ts
+++ leadout/sst.config.ts
@@ -5,10 +5,17 @@ export default {
   config(_input) {
     return {
       name: "leadout",
-      region: "us-east-1",
+      region: "us-east-2",
+      profile: "dev",
     };
   },
   stacks(app) {
-    app.stack(API)
+    app.setDefaultFunctionProps({
+      runtime: "nodejs18.x",
+      architecture: "arm_64",
+    })
+
+    app
+      .stack(API)
   },
 } satisfies SSTConfig;
