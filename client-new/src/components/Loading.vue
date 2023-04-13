290c39ddac29d2054c7cdc27b1d32b647d051565
Selena Strain <selena.strain@gmail.com>
2023-04-13 13:58:37 -0400

    Added loading indicator component

---
+++ client-new/src/components/Loading.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="loading">
+		<div class="loading__inner">
+			<img src="@/assets/images/pulse.gif" alt="Loading..." />
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.loading {
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100%;
+	z-index: 1;
+}
+</style>
